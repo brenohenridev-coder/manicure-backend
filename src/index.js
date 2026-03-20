943fb592dbf57114dfadc59f3b717aa0a44e3ffb
brenohenridev-coder <brenohenridev@gmail.com>
2026-03-20 01:49:36 -0300

    att

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ const licenseRoutes = require('./routes/license');
 const app = express();
 
 app.use(cors({ origin: process.env.FRONTEND_URL || '*', credentials: true }));
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 app.get('/health', (req, res) => {
   res.json({ status: 'ok', message: '💅 Manicure API running!' });
